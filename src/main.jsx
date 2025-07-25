993e43a5d05ee349b401d520f1661e6418c3baef
sandipv2 <sandiipvarma@gmail.com>
2025-07-25 20:43:12 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import App from './App.jsx'
-import ProductDetails from './components/Products/ProductDetails.jsx'
-import Error404 from './components/NotFound/Error404.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import App from './App.jsx';
+import ProductDetails from './components/Products/ProductDetails.jsx';
+import Error404 from './components/NotFound/Error404.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
+    <Route path='/' element={<Layout />}>
       <Route path='' element={<App />} />
       <Route path='/products/:product' element={<ProductDetails />} />
       <Route path='*' element={<Error404 />}></Route>
@@ -19,6 +20,7 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
+    <Analytics />
   </StrictMode>,
 )
